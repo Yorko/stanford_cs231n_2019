143d124fb129224e5a7f1d49dd9f58d6991ffb4c
vad4msiu <vad4msiu@gmail.com>
2019-06-10 13:03:52 +0300

    Fix 'adam' learning rate method
    Start using an Iteration number (config['t'])

--- cs231n/optim.py
+++ cs231n/optim.py
@@ -133,10 +133,15 @@ def adam(x, dx, config=None):
     # TODO: Implement the Adam update formula, storing the next value of x in #
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
-    ###########################################################################
+    ###########################################################################  
+    
+    config['t'] += 1
     config['m'] = config['beta1'] * config['m'] + (1 - config['beta1']) * dx
     config['v'] = config['beta2'] * config['v'] + (1 - config['beta2']) * (dx ** 2)
-    next_x = x - config['learning_rate'] * config['m'] / (np.sqrt(config['v'] + config['epsilon']))
+    mt = config['m'] / (1 - config['beta1']**config['t'])
+    vt = config['v'] / (1 - config['beta2']**config['t'])
+    next_x = x - config['learning_rate'] * mt / (np.sqrt(vt) + config['epsilon'])
+
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
